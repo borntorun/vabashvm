65de63a888edaf55143a05e2ca6a72467f31fb40
borntorun <jmmtcarvalho@gmail.com>
2015-02-02 11:16:55 +0000

    correct hostnam.sh provision script

--- provision/scripts/hostname.sh
+++ provision/scripts/hostname.sh
@@ -4,8 +4,8 @@
 ## Author: João Carvalho
 ## https://raw.githubusercontent.com/borntorun/vabashvm/master/LICENSE
 ## ======================================================================
-## Configure hostname 
-## 
+## Configure hostname
+##
 ## $1 hostname
 ## $2 ip address to configure on hosts file
 ##
@@ -24,34 +24,36 @@ output()
 	(printf "\n\t$(date +"%H:%M:%S"):==>$_thispackage:";	printf "$@")
 }
 
-util_isvalidip() #@ USAGE: util_isvalidip DOTTED-QUAD 
-{ 
+util_isvalidip() #@ USAGE: util_isvalidip DOTTED-QUAD
+{
 	#this function credits: Pro Bash Programming by Chris F.A. Johnson http://cfajohnson.com/books/cfajohnson/pbp/
-	case $1 in 
-	## reject the following: 
-	## empty string 
-	## anything other than digits and dots 
-	## anything not ending in a digit 
-	"" | *[!0-9.]* | *[!0-9]) echo 1; exit ;; 
-	esac 
-	## Change IFS to a dot, but only in this function 
-	local IFS=. 
-	## Place the IP address into the positional parameters; 
-	## after word splitting each element becomes a parameter 
-	set -- $1 	
-	[ $# -eq 4 ] && ## must be four parameters 
-	## each must be less than 256 
-	## A default of 666 (which is invalid) is used if a parameter is empty 
-	## All four parameters must pass the test 
-	[ ${1:-666} -le 255 ] && [ ${2:-666} -le 255 ] && 
+	case $1 in
+	## reject the following:
+	## empty string
+	## anything other than digits and dots
+	## anything not ending in a digit
+	"" | *[!0-9.]* | *[!0-9]) echo 1; exit ;;
+	esac
+	## Change IFS to a dot, but only in this function
+	local IFS=.
+	## Place the IP address into the positional parameters;
+	## after word splitting each element becomes a parameter
+	set -- $1
+	[ $# -eq 4 ] && ## must be four parameters
+	## each must be less than 256
+	## A default of 666 (which is invalid) is used if a parameter is empty
+	## All four parameters must pass the test
+	[ ${1:-666} -le 255 ] && [ ${2:-666} -le 255 ] &&
 	[ ${3:-666} -le 255 ] && [ ${4:-666} -le 255 ] && echo 0 && exit
-	echo 1	
-} 
+	echo 1
+}
 
 if [[ $# -eq 2 ]] && [[ $(util_isvalidip "$2") -eq 0 ]]
 then
 	printf "\nHOSTNAME=%s" "$1" >> /etc/sysconfig/network
 	printf "\n%s %s" "$2" "$1" >> /etc/hosts
+	printf "%s" "$1" > /etc/hostname
+	systemctl restart network
 else
 	output "Not configuring - incorrect parameters."
 fi

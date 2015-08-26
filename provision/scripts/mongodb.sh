d18ff67e5afe645f7a81ed282adfe378a00ad883
borntorun <jmmtcarvalho@gmail.com>
2015-08-26 23:39:52 +0100

    add package to install mongodb v3.0 / add config features on package mongodb.sh

--- provision/scripts/mongodb.sh
+++ provision/scripts/mongodb.sh
@@ -8,7 +8,7 @@
 ## Install MongoDB
 ##
 ## $1 - version to install [32|64 default]
-## 
+##
 ## This script was tested sucessfully in:
 ## 		CentOS 7 (64bit)
 ## ======================================================================
@@ -49,24 +49,31 @@ EOF
 		output "Configuring..."
 
 		sed -i "s|^enabled=1$|enabled=0|g" /etc/yum.repos.d/mongodb.repo
-		[[ ! $? -eq 0 ]] && output "Error configuring repo."
+		[[ ! $? -eq 0 ]] && output "......Error configuring repo."
 
 		sed -i "s|^bind_ip=.*$|bind_ip=0.0.0.0|" /etc/mongod.conf
-		[[ ! $? -eq 0 ]] && output "Error configuring bind_ip."
+		[[ ! $? -eq 0 ]] && output "......Error configuring bind_ip."
 
+    cat <<EOF >>/etc/yum.conf
+exclude=mongodb-org,mongodb-org-server,mongodb-org-shell,mongodb-org-mongos,mongodb-org-tools
+EOF
+    [[ ! $? -eq 0 ]] && output "......Error configuring yum.conf with exclude."
+
+    output "Configuring SELINUX..."
 		which semanage >/dev/null 2>&1
-		[[ ! $? -eq 0 ]] && output "Installing configuring dependencies ..." && yum -y install policycoreutils-python >/dev/null
+		[[ ! $? -eq 0 ]] && output "......Installing configuring dependencies ..." && yum -y install policycoreutils-python >/dev/null
 
 		[[ $? -eq 0 ]] && semanage port -a -t mongod_port_t -p tcp 27017  >/dev/null
+    output "......Ok."
 
 		output "Configuring firewall..."
 		firewall-cmd --permanent --zone=public --add-port=27017/tcp >/dev/null && firewall-cmd --permanent --zone=public --add-port=28017/tcp >/dev/null && firewall-cmd --reload >/dev/null
-		[[ ! $? -eq 0 ]] && output "Error configuring firewall."
+		([[ ! $? -eq 0 ]] && output "......Error configuring firewall.") || output "......Ok."
 
 		output "Starting service..."
 		chkconfig --levels 2345 mongod on
 		systemctl start mongod >/dev/null
-		[[ ! $? -eq 0 ]] && output "Error starting service." || output "Service running."
+		([[ ! $? -eq 0 ]] && output "......Error starting service.") || output "......Service is running."
 	}
 }
 
---
+++ provision/scripts/mongodb3.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+## ======================================================================
+## vabashvm - https://github.com/borntorun/vabashvm
+## Author: João Carvalho
+## https://raw.githubusercontent.com/borntorun/vabashvm/master/LICENSE
+## ======================================================================
+## Install MongoDB v. 3.0
+##
+## $1 - redhat system version to install [6|7 default]
+##
+## This script was not entirely tested yet sucessfully in:
+## 		CentOS 7 (64bit)
+## ======================================================================
+
+# if an error occurs stops
+#set -e
+: ${_thispackage="MongoDB"}
+: ${_thisfilename=${0##*/}}
+printf "\nvabashvm:$(date +"%H:%M:%S"):==>$_thispackage:Running [%s]..." "$0"
+#printf -- "[%s]-" $*
+output()
+{
+	(printf "\n\t$(date +"%H:%M:%S"):==>$_thispackage:";	printf "$@")
+}
+
+[[ ! "$1" == "6" ]] && [[ ! "$1" == "7" ]] && output "Invalid version." || {
+
+  which mongod >/dev/null 2>&1
+  [[ $? -eq 0 ]] && output "Already installed. Nothing to do." || {
+
+    : ${version="7"}
+
+    [[ ! $1 ]] && output "No platform version choosen..." || ([[ "$1" == "6" ]] && version="6")
+
+
+    output "Installing platform version [%s]..."  "$version"
+
+	  cat <<EOF >/etc/yum.repos.d/mongodb-org-3.0.repo
+[mongodb]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/${version}/mongodb-org/3.0/x86_64/
+gpgcheck=0
+enabled=1
+EOF
+
+    yum clean metadata >/dev/null
+    yum -y install mongodb-org >/dev/null
+    [[ ! $? -eq 0 ]] && output "Error in installation." || {
+
+      output "Package installed."
+      output "Configuring..."
+
+      sed -i "s|^enabled=1$|enabled=0|g" /etc/yum.repos.d/mongodb-org-3.0.repo
+      [[ ! $? -eq 0 ]] && output "......Error configuring repo."
+
+      sed -i "s|^bind_ip=.*$|bind_ip=0.0.0.0|" /etc/mongod.conf
+      [[ ! $? -eq 0 ]] && output "......Error configuring bind_ip."
+
+      cat <<EOF >>/etc/yum.conf
+exclude=mongodb-org,mongodb-org-server,mongodb-org-shell,mongodb-org-mongos,mongodb-org-tools
+EOF
+      [[ ! $? -eq 0 ]] && output "......Error configuring yum.conf with exclude."
+
+      output "Configuring SELINUX..."
+      which semanage >/dev/null 2>&1
+      [[ ! $? -eq 0 ]] && output "......Installing configuring dependencies ..." && yum -y install policycoreutils-python >/dev/null
+
+      [[ $? -eq 0 ]] && semanage port -a -t mongod_port_t -p tcp 27017  >/dev/null
+      output "......Ok."
+
+      output "Configuring firewall..."
+      firewall-cmd --permanent --zone=public --add-port=27017/tcp >/dev/null && firewall-cmd --permanent --zone=public --add-port=28017/tcp >/dev/null && firewall-cmd --reload >/dev/null
+      ([[ ! $? -eq 0 ]] && output "......Error configuring firewall.") || output "......Ok."
+
+      output "Starting service..."
+      chkconfig --levels 2345 mongod on
+      systemctl start mongod >/dev/null
+      ([[ ! $? -eq 0 ]] && output "......Error starting service.") || output "......Service is running."
+    }
+  }
+}
+
+printf "\nvabashvm:$(date +"%H:%M:%S"):==>$_thispackage:End [%s]." "$0"
+
+exit 0
